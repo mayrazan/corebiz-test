ced8a3d1eebd02ece1bfd5ddfb728621d8fc04fb
mayrazan <mayramanchein@yahoo.com.br>
2021-01-16 17:00:38 -0300

    Adicionando header na home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import { Footer } from "../../components/Footer/Footer";
+import { Header } from "../../components/Header/Header";
 
 export function Home() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   );
