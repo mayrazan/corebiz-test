0474d88fc80c7123ab5af5b331592dd8cca81b3e
mayrazan <mayramanchein@yahoo.com.br>
2021-01-17 16:45:44 -0300

    Criando cadastro da requisição e mostrando resposta

---
+++ src/services/Newsletter.js
@@ -0,0 +1,27 @@
+import { NEWSLETTER_API } from "./api";
+
+export async function createUser(userForm) {
+  const userFormJson = JSON.stringify(userForm);
+  const options = {
+    method: "post",
+    body: userFormJson,
+    headers: { "content-type": "application/json" },
+  };
+  // const proxyURL = "https://cors-anywhere.herokuapp.com/";
+  const response = await fetch(NEWSLETTER_API, options).then((res) =>
+    res.json().then((data) => data)
+  );
+
+  return response;
+}
+
+export async function messageStatus(res) {
+  const response = await createUser(res);
+  if (response.status === "error") {
+    var errorCode = response.status;
+    var errorMessage = response.message;
+    return { data: { errorCode, errorMessage }, success: false };
+  } else {
+    return { data: response, success: true };
+  }
+}
