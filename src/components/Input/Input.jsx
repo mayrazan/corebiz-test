90fa1d792d989bb8b165e91ee8b5b0c412bec200
mayrazan <mayramanchein@yahoo.com.br>
2021-01-17 16:46:12 -0300

    Acrescentando mais props ao input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,18 @@
 export function Input(props) {
-  const { type, className, placeholder } = props;
+  const { type, className, placeholder, required, value, setValue } = props;
+
+  function onChange(event) {
+    setValue(event.target.value);
+  }
+
   return (
-    <input type={type} className={className} placeholder={placeholder}></input>
+    <input
+      type={type}
+      className={className}
+      placeholder={placeholder}
+      required={required}
+      value={value}
+      onChange={onChange}
+    ></input>
   );
 }
