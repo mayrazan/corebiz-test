cfac194395239a420f0a9600dafb5d1e39216ce0
mayrazan <mayramanchein@yahoo.com.br>
2021-01-17 16:47:00 -0300

    Adicionando newsletter na página

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import { CarouselImages } from "./CarouselImages/CarouselImages";
 import "./Main.scss";
 import { Products } from "./Products.jsx/Products";
+import { Newsletter } from "./Newsletter/Newsletter";
 
 export function Main() {
   return (
     <main className="main-container">
       <CarouselImages />
-      <Products></Products>
+      <Products />
+      <Newsletter />
     </main>
   );
 }
